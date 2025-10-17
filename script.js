1a37658a384de174cbb4915393ac015d2ac16df7
muzeyyensafi <mzynxsafi@gmail.com>
2025-10-17 20:45:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Sürpriz butonu
+document.getElementById('surpriseBtn').onclick = function() {
+  alert('Sürpriz! Zeynep Dila Safi seni çok seviyor 😍');
+}
+
+// Film butonu
+document.getElementById('filmbutton').onclick = function() {
+  alert('Favori filmler hazır! Hadi izle 🎬');
+}
+
+// Müzik butonu
+document.getElementById('musicBtn').onclick = function() {
+  alert('Müzik zamanı 🎶');
+}
